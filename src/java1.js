2e43761e23cdbabc6cd8e278594c47c616185ff8
Zubair Mirza <batch16.014@gmail.com>
2019-07-08 20:34:03 +0500

    update

---
+++ src/java1.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function java1() {
+    return (
+        <div>
+            <h1>
+                TalhaRizwat
+            </h1>
+        </div>
+    );
+}
+
+export default java1;
